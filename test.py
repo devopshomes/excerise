a7d618b2ef0acf54ae0ded1c515df1d64ae76e52
Devopshome <30820003+devopshomes@users.noreply.github.com>
2017-09-06 17:01:52 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,10 @@
+x=int(input("please enter 1st num:"))
+y=int(input("please enter 2nd num:"))
+z=int(input("please enter 3rd num:"))
+
+
+print("the max of three values is :")
+print (max(x,y,z))
+
+
+input("please press  to exit")
